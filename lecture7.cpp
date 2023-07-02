37454f734378feefff8347a064307fe456f88d91
shalusingh456 <136883953+shalusingh456@users.noreply.github.com>
2023-07-02 19:24:50 +0530

    Add files via upload

---
+++ lecture7.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int password;
+    int count = 0;
+    
+   /* while (1)
+    {
+        cout << "chocolates ";
+        count++;
+        
+        if (count > 100)
+            break;
+    }
+    
+    
+	
+
+	
+	cout<<"Enter the password : ";
+	cin>>password;
+	
+	
+	if(password<999999)
+	{
+		cout<<"The password does not meet the required conditions, Please enter the password again";
+		cin>>password;
+	}
+	
+	cout<<"The user has now entered a correct password"<<endl;*/
+
+
+int i;
+for(i=0; ;i++ )
+{
+	cout<<i<<"";
+	if(1>100)break;  // This statement gets executed when i is 101
+}
+return 0;
+}
